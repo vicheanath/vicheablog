cf39f3a0bb405226f823cec342f2c15ed67a2a3b
Vichea Nath <nathvichea1@gmail.com>
2022-11-12 15:16:13 +0700

    fiexed google

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,11 @@ export default function App({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Kantumruy+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3051490042018197"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
