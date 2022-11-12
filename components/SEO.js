c6d683144036ff868f3d37e1e226f8df7d77b8e5
Vichea Nath <nathvichea1@gmail.com>
2022-11-12 13:30:00 +0700

    google

--- components/SEO.js
+++ components/SEO.js
@@ -1,34 +1,42 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import { useRouter } from 'next/router'
 import siteMetadata from '@/data/siteMetadata'
 
 const CommonSEO = ({ title, description, ogType, ogImage, twImage, canonicalUrl }) => {
   const router = useRouter()
   return (
-    <Head>
-      <title>{title}</title>
-      <meta name="robots" content="follow, index" />
-      <meta name="description" content={description} />
-      <meta property="og:url" content={`${siteMetadata.siteUrl}${router.asPath}`} />
-      <meta property="og:type" content={ogType} />
-      <meta property="og:site_name" content={siteMetadata.title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:title" content={title} />
-      {ogImage.constructor.name === 'Array' ? (
-        ogImage.map(({ url }) => <meta property="og:image" content={url} key={url} />)
-      ) : (
-        <meta property="og:image" content={ogImage} key={ogImage} />
-      )}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content={siteMetadata.twitter} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={twImage} />
-      <link
-        rel="canonical"
-        href={canonicalUrl ? canonicalUrl : `${siteMetadata.siteUrl}${router.asPath}`}
-      />
-    </Head>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={description} />
+        <meta property="og:url" content={`${siteMetadata.siteUrl}${router.asPath}`} />
+        <meta property="og:type" content={ogType} />
+        <meta property="og:site_name" content={siteMetadata.title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        {ogImage.constructor.name === 'Array' ? (
+          ogImage.map(({ url }) => <meta property="og:image" content={url} key={url} />)
+        ) : (
+          <meta property="og:image" content={ogImage} key={ogImage} />
+        )}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={siteMetadata.twitter} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={twImage} />
+        <link
+          rel="canonical"
+          href={canonicalUrl ? canonicalUrl : `${siteMetadata.siteUrl}${router.asPath}`}
+        />
+      </Head>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3051490042018197"
+        crossOrigin="anonymous"
+      ></Script>
+    </>
   )
 }
 
