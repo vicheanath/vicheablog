d5c653373244a8671613b177a3087597ff3fd511
Vichea <nathvichea1@gmail.com>
2023-06-28 23:54:17 +0700

    Add more data to tools

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -24,15 +24,17 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
 
   return (
     <a
-      className="text-sm text-gray-500 transition hover:text-gray-600"
+      className="text-sm text-gray-500 transition hover:text-yellow-500 dark:text-gray-400 dark:hover:text-yellow-500"
       target="_blank"
       rel="noopener noreferrer"
       href={href}
     >
-      <span className="sr-only">{kind}</span>
-      <SocialSvg
-        className={`fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-${size} w-${size}`}
-      />
+      <span className="inline-flex rounded-md bg-gray-100 p-3 shadow-sm hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <span className="sr-only">{kind}</span>
+        <SocialSvg
+          className={`fill-current text-gray-700  dark:text-gray-200 h-${size} w-${size}`}
+        />
+      </span>
     </a>
   )
 }
