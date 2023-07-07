ad4a9ea4f14085886198092670aff44ed7db84a8
Vichea <nathvichea1@gmail.com>
2023-07-08 00:05:30 +0700

    Fiexd OAuth

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -93,7 +93,7 @@ const Profile = () => {
   )
 }
 
-const LayoutWrapper = ({ children }) => {
+const LayoutWrapper = ({ children, session }) => {
   const router = useRouter()
   const path = router.pathname
   const activeMenuItem = (href) => {
@@ -106,45 +106,47 @@ const LayoutWrapper = ({ children }) => {
 
   return (
     <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
+      <SessionProvider session={session}>
+        <div className="flex h-screen flex-col justify-between">
+          <header className="flex items-center justify-between py-10">
+            <div>
+              <Link href="/" aria-label={siteMetadata.headerTitle}>
+                <div className="flex items-center justify-between">
+                  <div className="mr-3">
+                    <Logo />
                   </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className={`p-1 font-medium sm:p-4 ${activeMenuItem(link.href)}`}
-                >
-                  <span className="text-gray-900 dark:text-gray-100">{link.icon}</span>
-                  <span className="ml-2">{link.title}</span>
-                </Link>
-              ))}
+                  {typeof siteMetadata.headerTitle === 'string' ? (
+                    <div className="hidden h-6 text-2xl font-semibold sm:block">
+                      {siteMetadata.headerTitle}
+                    </div>
+                  ) : (
+                    siteMetadata.headerTitle
+                  )}
+                </div>
+              </Link>
             </div>
-            <ThemeSwitch />
-            {/* <Profile /> */}
-            <MobileNav />
-          </div>
-        </header>
-        <main className="mb-auto">{children}</main>
-        <Footer />
-      </div>
+            <div className="flex items-center text-base leading-5">
+              <div className="hidden sm:block">
+                {headerNavLinks.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.href}
+                    className={`p-1 font-medium sm:p-4 ${activeMenuItem(link.href)}`}
+                  >
+                    <span className="text-gray-900 dark:text-gray-100">{link.icon}</span>
+                    <span className="ml-2">{link.title}</span>
+                  </Link>
+                ))}
+              </div>
+              <ThemeSwitch />
+              {/* <Profile /> */}
+              <MobileNav />
+            </div>
+          </header>
+          <main className="mb-auto">{children}</main>
+          <Footer />
+        </div>
+      </SessionProvider>
     </SectionContainer>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
-import { SessionProvider } from 'next-auth/react'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -27,7 +26,8 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
-      <LayoutWrapper>
+
+      <LayoutWrapper session={session} {...pageProps}>
         <Component {...pageProps} />
         <div id="modal-root"></div>
       </LayoutWrapper>
