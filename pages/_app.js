4a7a708da20d6a1d8469194e610490eca6fca1f4
Vichea <nathvichea1@gmail.com>
2023-07-07 23:23:55 +0700

    Fiexd OAuth

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-
 import '@fontsource/inter/variable-full.css'
-
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
@@ -17,31 +14,25 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
-  console.log(session)
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <SessionProvider session={session} refetchInterval={60}>
-        <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Kantumruy+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-            rel="stylesheet"
-          />
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3051490042018197"
-            crossOrigin="anonymous"
-          ></script>
-        </Head>
-        {isDevelopment && isSocket && <ClientReload />}
-        <Analytics />
-        <LayoutWrapper {...pageProps}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kantumruy+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      {isDevelopment && isSocket && <ClientReload />}
+      <Analytics />
+      <LayoutWrapper {...pageProps}>
+        <SessionProvider session={session}>
           <Component {...pageProps} />
           <div id="modal-root"></div>
-        </LayoutWrapper>
-      </SessionProvider>
+        </SessionProvider>
+      </LayoutWrapper>
     </ThemeProvider>
   )
 }
