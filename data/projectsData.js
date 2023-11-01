441549b5fbd3005d26928b20c6256df971853249
Vichea <48352653+vicheanath@users.noreply.github.com>
2023-10-31 20:17:55 -0500

    added : project to nav

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,12 +1,18 @@
 const projectsData = [
   {
-    title: 'Location Management System',
-    description: `Location Management System is a web application that allows users to manage locations and their details such as name, address.`,
-    imgSrc: '/static/images/google.png',
+    title: 'Marching App UX/UI Design',
+    description: `Marching App UX/UI Design is a mobile application that allows users to find a partner to play sports.`,
+    imgSrc: '/static/images/project/maching-app.jpg',
     href: 'https://www.google.com',
   },
   {
     title: 'Online Cash Management System',
+    description: `Online Cash Management System is a web application that allows users to manage cash in game online such as SPORTS, CASINO, SLOT, POKER, ...`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Customer Service Monthly Report System',
     description: `Online Cash Management System is a web application that allows users to manage cash in game online such as SPORTS, CASINO, SLOT, POKER, ...`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
